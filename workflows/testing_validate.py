8438ecf6f7603222b091c013093b42e7c929d20c
Tosin <aoluwatosin10@gmail.com>
2020-05-06 20:59:24 +0000

    Add working validator

--- workflows/testing_validate.py
+++ workflows/testing_validate.py
@@ -1,3 +1,5 @@
+#!/home/gns3/virtualvenvs/nornir/bin/python
+
 from nornir import InitNornir
 from nornir.core.filter import F
 from nornir.plugins.functions.text import print_result
@@ -5,14 +7,13 @@ from nornir.plugins.tasks import networking, text
 
 
 def main():
-    nr = InitNornir(config_file="config.yaml")
+    nr = InitNornir(config_file="../config.yaml")
     devices = nr.filter(F(platform="junos"))
     # import ipdb
 
     # ipdb.set_trace()
     result = devices.run(task=networking.napalm_validate,
-                         src="validate_ipaddr.yaml",)
-    # result = devices.run(task=networking.napalm_cli, commands=["show version"])
+                         src="../validators/validate.yaml",)
     print_result(result)
 
 
